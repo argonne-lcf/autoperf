eee2d2c6bab31c1dd46ded7f53b32ee6bd451883
Kevin Harms <harms@alcf.anl.gov>
2018-01-25 16:29:26 +0000

    Initial buildable code

--- crayxc/darshan-apxc-log-format.h
+++ crayxc/darshan-apxc-log-format.h
@@ -10,45 +10,48 @@
 /* current AutoPerf Cray XC log format version */
 #define DARSHAN_APXC_VER 1
 
-#define APXC_COUNTERS \
-    /* counter 1 */\
-    X(APXC_COUNTER1) \
+#define APXC_RTR_COUNTERS \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC0) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC1) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC2) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC3) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC4) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC5) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC6) \
+    X(AR_RTR_0_0_INQ_PRF_INCOMING_FLIT_VC7) \
+    X(AR_RTR_0_0_INQ_PRF_ROWBUS_STALL_CNT) \
     /* end of counters */\
-    X(APXC_NUM_INDICES)
-
-#define AP_CRAYXC_F_COUNTERS \
-    /* timestamp when data was collected */\
-    X(APXC_F_TIMESTAMP) \
-    /* end of counters */\
-    X(APXC_F_NUM_INDICES)
+    X(APXC_RTR_NUM_INDICES)
 
 #define X(a) a,
 /* integer counters for the "BGQ" example module */
-enum darshan_apxc_indices
+enum darshan_apxc_rtr_indices
 {
-    APXC_COUNTERS
-};
-
-/* floating point counters for the "AutoPerf Cray XC" module */
-enum darshan_apxc_f_indices
-{
-    APXC_F_COUNTERS
+    APXC_RTR_COUNTERS
 };
 #undef X
 
-/* the darshan_apxc_record structure encompasses the high-level data/counters
+/* the darshan_apxc_router_record structure encompasses the data/counters
  * which would actually be logged to file by Darshan for the AP Cray XC
  * module. This example implementation logs the following data for each
  * record:
  *      - a darshan_base_record structure, which contains the record id & rank
- *      - integer I/O counters (operation counts, I/O sizes, etc.)
- *      - floating point I/O counters (timestamps, cumulative timers, etc.)
+ *      - integer I/O counters 
+ *      - floating point I/O counters 
  */
-struct darshan_apxc_record
+struct darshan_apxc_router_record
 {
     struct darshan_base_record base_rec;
-    int64_t counters[APXC_NUM_INDICES];
-    double fcounters[APXC_F_NUM_INDICES];
+    int64_t coord[4]; /* ugroup, uchassis, ublade, unode */
+    int64_t counters[APXC_RTR_NUM_INDICES];
+};
+
+struct darshan_apxc_header_record
+{
+    struct darshan_base_record base_rec;
+    int64_t nblades;
+    int64_t nchassis;
+    int64_t ngroups;
 };
 
 #endif /* __DARSHAN_APXC_LOG_FORMAT_H */
