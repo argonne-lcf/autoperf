155b3cabe724e3a613d62858a6d837cde2a713bf
Sudheer Chunduri <sudheer@anl.gov>
2021-01-27 21:39:56 +0000

    updating the counter names python scripts

--- apmpi/util/darshan-apmpi-logutils.h
+++ apmpi/util/darshan-apmpi-logutils.h
@@ -8,8 +8,9 @@
 #define __APMPI_LOG_UTILS_H
 
 extern char *apmpi_counter_names[];
-extern char *apmpi_fcounter_names[];
-
+extern char *apmpi_f_mpiop_totaltime_counter_names[]; 
+extern char *apmpi_f_mpiop_totalsync_counter_names[];
+extern char *apmpi_f_mpi_global_counter_names[];
 extern struct darshan_mod_logutil_funcs apmpi_logutils;
 
 #endif
--- apmpi/util/print-apmpi-size.c
+++ apmpi/util/print-apmpi-size.c
@@ -6,6 +6,9 @@ int main (int argc, char **argv)
 {
    printf ("APMPI_NUM_INDICES = %d\n", APMPI_NUM_INDICES);
    printf ("APMPI_F_NUM_INDICES = %d\n", APMPI_F_NUM_INDICES);
+   printf ("APMPI_F_SYNC_NUM_INDICES = %d\n", APMPI_F_SYNC_NUM_INDICES);
+   printf ("APMPI_F_GLOBAL_NUM_INDICES = %d\n", APMPI_F_GLOBAL_NUM_INDICES);
+   printf ("sizeof darshan_apmpi_header_record = %d\n", sizeof(struct darshan_apmpi_header_record));
    printf ("sizeof darshan_apmpi_perf_record = %d\n", sizeof(struct darshan_apmpi_perf_record));
 
    return 0;
