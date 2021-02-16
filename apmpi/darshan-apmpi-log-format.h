22ab5577d6aa2ec79c2c1cad60efe53af30a36a8
Sudheer Chunduri <sudheer@anl.gov>
2021-02-16 15:07:28 +0000

    experimental release of APMPI 1.0

--- apmpi/darshan-apmpi-log-format.h
+++ apmpi/darshan-apmpi-log-format.h
@@ -10,7 +10,7 @@
 #define MPI_MAX_PROCESSOR_NAME 128
 #endif
 /* current AutoPerf MPI log format version */
-#define APMPI_VER 0.1
+#define APMPI_VER 1
 
 #define APMPI_MAGIC ('A'*0x100000000+\
                             'P'*0x1000000+\
